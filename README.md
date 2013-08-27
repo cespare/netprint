3703de9eac1428af7c8b91c3f42c076431e90299
Caleb Spare <cespare@gmail.com>
2013-08-27 13:56:57 -0700

    Implement TCP and UDP handling

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 A pretty-printer for network requests.
 
+## Notes
+
+netprint only handles a single connection at a time, to ensure the output is easy to understand.
+
 ## To Do
 
 * Nice colorized printing
-* `-tcp`, for printing raw TCP requests
-* `-udp`, for printing raw UDP packets
 * `-v`, verbose mode -- when serving HTTP, print everything in the request (headers, etc).
 * Pretty-print json (if valid)
 * `-x` for hex dumping the result instead of printing directly. Also hex-dump automatically if the result is a
--- netprint.go
+++ netprint.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"sync"
@@ -13,14 +14,19 @@ var (
 	addr         = flag.String("addr", "localhost:7702", "The address on which netprint listens.")
 	tcp          = flag.Bool("tcp", false, "Accept raw TCP requests instead of HTTP.")
 	udp          = flag.Bool("udp", false, "Accept raw UDP packets instead of HTTP.")
-	responseCode = flag.Int("response-code", http.StatusOK, "HTTP response code")
-	responseText = flag.String("response-text", "", "Response body for TCP/HTTP requests")
+	responseCode = flag.Int("response-code", http.StatusOK, "Response code for HTTP requests.")
+	responseText = flag.String("response-text", "", "Response body for HTTP requests.")
 	mode         = modeHTTP
 	mut          sync.Mutex
 )
 
 func fatal(args ...interface{}) {
 	fmt.Println(args...)
+	os.Exit(1)
+}
+
+func fatalf(format string, args ...interface{}) {
+	fmt.Printf(format, args...)
 	os.Exit(1)
 }
 
@@ -39,11 +45,10 @@ func init() {
 	setFlags := map[string]bool{}
 	flag.Visit(func(f *flag.Flag) { setFlags[f.Name] = true })
 
-	if setFlags["response-code"] && mode != modeHTTP {
-		fatal("Cannot specify -response-code except in HTTP mode.")
-	}
-	if setFlags["response-text"] && mode == modeUDP {
-		fatal("-response-text doesn't make sense with -udp.")
+	for _, f := range []string{"response-code", "response-text"} {
+		if setFlags[f] && mode != modeHTTP {
+			fatalf("Cannot specify -%s except in HTTP mode.", f)
+		}
 	}
 	if setFlags["response-code"] && (*responseCode < 100 || *responseCode >= 600) {
 		fatal("Invalid HTTP response code:", *responseCode)
@@ -114,8 +119,70 @@ func handleHTTP(w http.ResponseWriter, r *http.Request) {
 func runHTTP() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleHTTP)
-	fmt.Println("Now listening on", *addr)
+	fmt.Println("Now accepting HTTP requests on", *addr)
 	return http.ListenAndServe(*addr, mux)
+}
+
+func handleTCP(conn net.Conn) {
+	fmt.Printf(">>>>> %s connected.\n", conn.RemoteAddr())
+	written, err, endingNewline := copyRecordNewline(os.Stdout, conn)
+	if err != nil {
+		return
+	}
+	if written == 0 {
+		fmt.Println("(No data transmitted.)")
+	} else if !endingNewline {
+		fmt.Println()
+	}
+	fmt.Printf(">>>>> %s disconnected.\n", conn.RemoteAddr())
+}
+
+func runTCP() error {
+	l, err := net.Listen("tcp", *addr)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Now accepting raw TCP requests on", *addr)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		handleTCP(conn)
+	}
+}
+
+func handleUDP(conn *net.UDPConn) {
+	buf := make([]byte, 10 * 1024) // 10KB buffer to handle pretty damn big UDP packets
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			return
+		}
+		fmt.Printf(">>>>> Received a packet from %s\n", addr)
+		if n == 0 {
+			fmt.Println("(No data transmitted.)")
+		} else {
+			os.Stdout.Write(buf[:n])
+			if buf[n-1] != '\n' {
+				fmt.Println()
+			}
+		}
+	}
+}
+
+func runUDP() error {
+	u, err := net.ResolveUDPAddr("udp", *addr)
+	if err != nil {
+		return err
+	}
+	conn, err := net.ListenUDP("udp", u)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Now accepting raw UDP requests on", *addr)
+	handleUDP(conn)
+	return nil
 }
 
 func main() {
@@ -123,8 +190,8 @@ func main() {
 	case modeHTTP:
 		fatal(runHTTP())
 	case modeTCP:
-		fatal("TCP not implemented.")
+		fatal(runTCP())
 	case modeUDP:
-		fatal("UDP not implemented")
+		fatal(runUDP())
 	}
 }
